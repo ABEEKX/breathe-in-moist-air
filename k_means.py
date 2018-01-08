ac449bf2de67fce8c28b3c4ed4ed4b7f04307816
hjhjpark <hjhjpark94@naver.com>
2018-01-08 16:24:38 +0900

    Add files via upload

--- k_means.py
+++ k_means.py
@@ -5,8 +5,8 @@ import pymysql
 import csv
 
 n_features = 2
-n_clusters = 5
-n_samples_per_cluster = 50000
+n_clusters = 3
+n_samples_per_cluster = 100
 seed = 700
 
 #db connect
@@ -27,14 +27,10 @@ def choose_random_centroids(samples, n_clusters):
 
 def assign_to_nearest(samples, centroids):
     # Finds the nearest centroid for each sample
-
-    # START from http://esciencegroup.com/2016/01/05/an-encounter-with-googles-tensorflow/
     expanded_vectors = tf.expand_dims(samples, 0)
     expanded_centroids = tf.expand_dims(centroids, 1)
-    distances = tf.reduce_sum( tf.square(
-               tf.subtract(expanded_vectors, expanded_centroids)), 2)
+    distances = tf.reduce_sum( tf.square(tf.subtract(expanded_vectors, expanded_centroids)), 2)
     mins = tf.argmin(distances, 0)
-    # END from http://esciencegroup.com/2016/01/05/an-encounter-with-googles-tensorflow/
     nearest_indices = mins
     return nearest_indices
 
@@ -45,24 +41,15 @@ def update_centroids(samples, nearest_indices, n_clusters):
     new_centroids = tf.concat([tf.expand_dims(tf.reduce_mean(partition, 0), 0) for partition in partitions], 0)
     return new_centroids
 
+cursor.execute("SELECT pm10,pm25 FROM sensors")
+samples=[]
+for row in cursor:
+    samples.append(tf.constant([float(row[0]),float(row[1])],dtype=tf.float32))
 
-def plot_clusters(all_samples, centroids, n_samples_per_cluster):
-    import matplotlib.pyplot as plt
-    # Plot out the different clusters
-    # Choose a different colour for each cluster
-    colour = plt.cm.rainbow(np.linspace(0, 1, len(centroids)))
+# disconnect db
+cursor.close()
+con.close()
 
-
-    for i, centroid in enumerate(centroids):
-    # Grab just the samples fpr the given cluster and plot them out with a new colour
-        samples = all_samples[i * n_samples_per_cluster:(i + 1) * n_samples_per_cluster]
-        plt.scatter(samples[:, 0], samples[:, 1], c=colour[i])
-    # Also plot centroid
-        plt.plot(centroid[0], centroid[1], markersize=35, marker="x", color='k', mew=10)
-        plt.plot(centroid[0], centroid[1], markersize=30, marker="x", color='m', mew=5)
-    plt.show()
-cursor.execute("SELECT pm10,ppm FROM sensors")
-samples=list(cursor)
 initial_centroids = choose_random_centroids(samples, n_clusters)
 nearest_indices = assign_to_nearest(samples, initial_centroids)
 updated_centroids = update_centroids(samples, nearest_indices, n_clusters)
@@ -73,7 +60,4 @@ with tf.Session() as session:
     updated_centroid_value = session.run(updated_centroids)
     print(updated_centroid_value)
 
-plot_clusters(sample_values, updated_centroid_value, n_samples_per_cluster)
-cursor.close()
-con.close()
 
