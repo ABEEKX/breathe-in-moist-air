f5f5b2f440964a40eecc5944e172c419ddf3acb3
hjhjpark <hjhjpark94@naver.com>
2018-01-09 18:49:00 +0900

    Add files via upload

--- rnn.py
+++ rnn.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 import matplotlib
 import os
 import pymysql
+
 
 tf.set_random_seed(700)
 
@@ -10,10 +12,9 @@ if "DISPLAY" not in os.environ:
     # remove Travis CI Error
     matplotlib.use('Agg')
 
-import matplotlib.pyplot as plt
-
 
 def MinMaxScaler(data):
+    #  Min Max Normalization
     numerator = data - np.min(data, 0)
     denominator = np.max(data, 0) - np.min(data, 0)
     # noise term prevents the zero division
@@ -26,23 +27,33 @@ output_dim = 1
 learning_rate = 0.01
 iterations = 500
 
+# connect db
 con=pymysql.connect(host='52.78.192.119',port=3306,user='root',password='Cap2bowoo!',db='abeekx',charset='utf8')
 cursor=con.cursor()
 cursor.execute("SELECT temp FROM sensors")
+xy=[]
+for row in cursor:
+    xy.append([float(row[0])])
+
+cursor.close()
+con.close()  # db disconnect
+
+xy=xy[::-1]  # reverse data
+xy1=xy  # pre Scalar data
+numerator = xy - np.min(xy, 0)  # MinMaxScalar
+denominator = np.max(xy, 0) - np.min(xy, 0)
+xy =  (xy - np.min(xy, 0))/ (denominator + 1e-7)
+x = xy
+y = xy[:,[-1]]  # Close as label
 
 dataX = []
 dataY = []
-list(cursor).pop(0)
-temp=cursor
+for i in range(0, len(y) - seq_length):
+    _x = x[i:i + seq_length]
+    _y = y[i + seq_length]  # Next temperature
+    dataX.append(_x)
+    dataY.append(_y)
 
-for row in cursor:
-    dataX.append(float(row[0]))
-
-for row in temp:
-    dataY.append(float(row[0]))
-
-cursor.close()
-con.close()
 # train/test split
 train_size = int(len(dataY) * 0.7)
 test_size = len(dataY) - train_size
@@ -52,7 +63,7 @@ trainY, testY = np.array(dataY[0:train_size]), np.array(
     dataY[train_size:len(dataY)])
 
 # input place holders
-X = tf.placeholder(tf.float32, [None, 1])
+X = tf.placeholder(tf.float32, [None, seq_length, data_dim])
 Y = tf.placeholder(tf.float32, [None, 1])
 
 # build a LSTM network
@@ -60,7 +71,7 @@ cell = tf.contrib.rnn.BasicLSTMCell(
     num_units=hidden_dim, state_is_tuple=True, activation=tf.tanh)
 outputs, _states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
 Y_pred = tf.contrib.layers.fully_connected(
-    outputs[:, -1], output_dim, activation_fn=None)  # We use the last cell's output
+    outputs[:,-1], output_dim, activation_fn=None)  # reverse output
 
 # cost/loss
 loss = tf.reduce_sum(tf.square(Y_pred - Y))  # sum of the squares
@@ -76,11 +87,9 @@ rmse = tf.sqrt(tf.reduce_mean(tf.square(targets - predictions)))
 with tf.Session() as sess:
     init = tf.global_variables_initializer()
     sess.run(init)
-
     # Training step
     for i in range(iterations):
-        _, step_loss = sess.run([train, loss], feed_dict={
-                                X: trainX, Y: trainY})
+        _, step_loss = sess.run([train, loss], feed_dict={X: trainX, Y: trainY})
         print("[step: {}] loss: {}".format(i, step_loss))
 
     # Test step
@@ -90,7 +99,14 @@ with tf.Session() as sess:
     print("RMSE: {}".format(rmse_val))
 
     # Plot predictions
+    testY = testY*( np.max(xy1, 0) - np.min(xy1, 0) + 1e-7)+np.min(xy1, 0)  # Decode MinMaxScalar
     plt.plot(testY)
+    # 4 times added prediction
+    distance=(test_predict[-1]-test_predict[-2])*0.2  # Momentum
+    for i in range(5):
+        test_predict=np.append(test_predict,[test_predict[-1]+(i+1)*distance],axis=0)
+
+    test_predict=test_predict*( np.max(xy1, 0) - np.min(xy1, 0) + 1e-7)+np.min(xy1, 0)  # Decode MinMaxScalar
     plt.plot(test_predict)
     plt.xlabel("Time Period")
     plt.ylabel("Temperature")
