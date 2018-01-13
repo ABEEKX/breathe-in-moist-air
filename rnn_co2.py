5a61b43502861d2d688f3e619e67567e709247c6
hjhjpark <hjhjpark94@naver.com>
2018-01-13 18:21:40 +0900

    co2 prediction

---
+++ rnn_co2.py
@@ -0,0 +1,99 @@
+import tensorflow as tf
+import numpy as np
+import pymysql
+
+tf.set_random_seed(700)
+
+seq_length = 7
+data_dim = 1
+hidden_dim = 10
+output_dim = 1
+learning_rate = 0.01
+iterations = 2000
+#num_layers = 3
+
+# connect db
+con=pymysql.connect(host='52.78.192.119',port=3306,user='root',password='Cap2bowoo!',db='abeekx',charset='utf8')
+cursor=con.cursor()
+cursor.execute("SELECT ppm,time FROM sensors")
+
+xy=[]
+#for row in cursor:
+#    xy.append([float(row[0])])
+
+# noisy data handling
+flag=False
+for row in cursor:
+    cur_ppm = float(row[0])
+    if(flag):
+        if(cur_ppm-pre_ppm>3000.0):
+            xy.append([pre_ppm])
+            cur_ppm=pre_ppm
+        else:
+            xy.append([cur_ppm])
+    else:
+        xy.append([cur_ppm])
+    pre_ppm = cur_ppm
+    flag=True
+
+cursor.close()
+con.close()  # db disconnect
+
+xy1=xy  # pre Scalar data
+numerator = xy - np.min(xy, 0)  # MinMaxScalar
+denominator = np.max(xy, 0) - np.min(xy, 0)
+xy =  (xy - np.min(xy, 0))/ (denominator + 1e-7)
+x = xy
+
+dataX = []
+dataY = []
+for i in range(0, len(x) - seq_length):
+    _x = x[i:i + seq_length]
+    _y = x[i+seq_length]
+    dataX.append(_x)
+    dataY.append(_y)
+
+# train/test split
+train_size = int(0.8*len(dataX))
+trainX, testX = np.array(dataX[0:train_size]), np.array(dataX[train_size:len(dataX)])
+trainY, testY = np.array(dataY[0:train_size]), np.array(dataY[train_size:len(dataY)])
+
+# input place holders
+X = tf.placeholder(tf.float32, [None, seq_length, data_dim])
+Y = tf.placeholder(tf.float32, [None,  data_dim])
+
+# build a LSTM network
+cell = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_dim, state_is_tuple=True, activation=tf.tanh)
+'''
+# Multi Layer LSTM network
+cells = []
+for _ in range(num_layers):
+  cell = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_dim, state_is_tuple=True, activation=tf.tanh)
+  cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=0.8)
+  cells.append(cell)
+cell = tf.contrib.rnn.MultiRNNCell(cells,state_is_tuple=True)
+'''
+outputs, _states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
+Y_pred = tf.contrib.layers.fully_connected(outputs[:,-1], output_dim, activation_fn=None)
+
+# cost/loss
+loss = tf.reduce_sum(tf.square(Y_pred - Y))  # sum of the squares
+# optimizer
+optimizer = tf.train.AdamOptimizer(learning_rate)
+train = optimizer.minimize(loss)
+
+# RMSE
+targets = tf.placeholder(tf.float32, [None, data_dim])
+predictions = tf.placeholder(tf.float32, [None, data_dim])
+rmse = tf.sqrt(tf.reduce_mean(tf.square(targets - predictions)))
+
+sess=tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
+# Training step
+for i in range(iterations):
+    _, step_loss = sess.run([train, loss], feed_dict={X: trainX, Y: trainY})
+    print("[step: {}] loss: {}".format(i, step_loss))
+
+saver=tf.train.Saver()
+save_path=saver.save(sess,"./rnn_train_co2.ckpt")  # save train variable
---
+++ rnn_test_co2.py
@@ -0,0 +1,146 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import pymysql
+import datetime
+
+seq_length = 7
+data_dim = 1
+hidden_dim = 10
+output_dim = 1
+#num_layers = 3
+
+# connect db
+con=pymysql.connect(host='52.78.192.119',port=3306,user='root',password='Cap2bowoo!',db='abeekx',charset='utf8')
+cursor=con.cursor()
+cursor.execute("SELECT ppm,time FROM sensors")
+xy=[]
+time=[]
+#for row in cursor:
+#    xy.append([float(row[0])])
+#    time.append([str(row[1])])  # get timestamp
+
+# noisy data handling
+flag=False
+for row in cursor:
+    cur_ppm = float(row[0])
+    time.append([str(row[1])])  # get timestamp
+    if(flag):
+        if(cur_ppm-pre_ppm>3000.0):
+            xy.append([pre_ppm])
+            cur_ppm=pre_ppm
+        else:
+            xy.append([cur_ppm])
+    else:
+        xy.append([cur_ppm])
+    pre_ppm = cur_ppm
+    flag=True
+
+xy1=xy  # pre Scalar data
+numerator = xy - np.min(xy, 0)  # MinMaxScalar
+denominator = np.max(xy, 0) - np.min(xy, 0)
+xy = (xy - np.min(xy, 0))/ (denominator + 1e-7)
+x = xy
+
+dataX = []
+dataY = []
+for i in range(0, len(x) - seq_length):
+    _x = x[i:i + seq_length]
+    _y = x[i+seq_length]
+    dataX.append(_x)
+    dataY.append(_y)
+
+# train/test split
+train_size = int(0.95*len(dataX))
+trainX, testX = np.array(dataX[0:train_size]), np.array(dataX[train_size:len(dataX)])
+trainY, testY = np.array(dataY[0:train_size]), np.array(dataY[train_size:len(dataY)])
+timeY = np.array(time[train_size:len(dataY)])
+
+X = tf.placeholder(tf.float32, [None, seq_length, data_dim])
+Y = tf.placeholder(tf.float32, [None,  data_dim])
+
+# build a LSTM network
+cell = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_dim, state_is_tuple=True, activation=tf.tanh)
+'''
+# Multi Layer LSTM network
+cells = []
+for _ in range(num_layers):
+  cell = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_dim, state_is_tuple=True, activation=tf.tanh)
+  cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=0.8)
+  cells.append(cell)
+cell = tf.contrib.rnn.MultiRNNCell(cells,state_is_tuple=True)
+'''
+outputs, _states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
+Y_pred = tf.contrib.layers.fully_connected(outputs[:,-1], output_dim, activation_fn=None)
+
+targets = tf.placeholder(tf.float32, [None, data_dim])
+predictions = tf.placeholder(tf.float32, [None, data_dim])
+rmse = tf.sqrt(tf.reduce_mean(tf.square(targets - predictions)))
+
+sess=tf.Session()
+saver=tf.train.Saver()
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+save_path="./rnn_train_co2.ckpt"
+saver.restore(sess,save_path)  # restore train variable
+
+# Test step
+test_predict = sess.run(Y_pred, feed_dict={X: testX})
+
+# 5 times added prediction
+temp_list=testX[-1]
+temp_list=np.delete(temp_list, 0, axis=0)
+temp_list= np.append(temp_list, test_predict[-1, 0].reshape(1, 1), axis=0)
+test_predict1 = sess.run(Y_pred, feed_dict={X: temp_list.reshape(1, 7, 1)})
+temp_list=np.delete(temp_list, 0,axis=0)
+temp_list= np.append(temp_list, test_predict[-1, 0].reshape(1, 1), axis=0)
+test_predict2 = sess.run(Y_pred, feed_dict={X: temp_list.reshape(1, 7, 1)})
+temp_list=np.delete(temp_list, 0,axis=0)
+temp_list= np.append(temp_list, test_predict[-1, 0].reshape(1, 1), axis=0)
+test_predict3 = sess.run(Y_pred, feed_dict={X: temp_list.reshape(1, 7, 1)})
+temp_list=np.delete(temp_list, 0,axis=0)
+temp_list= np.append(temp_list, test_predict[-1, 0].reshape(1, 1), axis=0)
+test_predict4 = sess.run(Y_pred, feed_dict={X: temp_list.reshape(1, 7, 1)})
+temp_list=np.delete(temp_list, 0,axis=0)
+temp_list= np.append(temp_list, test_predict[-1, 0].reshape(1, 1), axis=0)
+test_predict5 = sess.run(Y_pred, feed_dict={X: temp_list.reshape(1, 7, 1)})
+
+rmse_val = sess.run(rmse, feed_dict={targets: testY, predictions: test_predict})
+print("RMSE: {}".format(rmse_val))
+
+test_predict=np.append(test_predict, test_predict1, axis=0)
+test_predict=np.append(test_predict, test_predict2, axis=0)
+test_predict=np.append(test_predict, test_predict3, axis=0)
+test_predict=np.append(test_predict, test_predict4, axis=0)
+test_predict=np.append(test_predict, test_predict5, axis=0)
+
+testY = testY*( np.max(xy1, 0) - np.min(xy1, 0) + 1e-7)+np.min(xy1, 0)  # Decode MinMaxScalar
+test_predict = test_predict*(np.max(xy1, 0) - np.min(xy1, 0) + 1e-7)+np.min(xy1, 0)
+
+# Plot predictions
+plt.plot(testY)
+plt.plot(test_predict)
+plt.xlabel("Time Period")
+plt.ylabel("CO2(ppm)")
+plt.show()
+
+
+
+f='%Y-%m-%d %H:%M:%S'  # time format
+# save db
+list_length=len(testY)
+for i in range(list_length):
+    cursor.execute("INSERT INTO predict_co2 (value1,value2,time) VALUES (%f,%f,'%s')"
+                   % (testY[i], test_predict[i], datetime.datetime.strptime(str(timeY[i, 0]), f)))
+
+for i in range(5):
+    cursor.execute("INSERT INTO predict_co2 (value2,time) VALUES (%f,'%s')"
+                   % (test_predict[list_length+i],
+                      datetime.datetime.strptime(str(timeY[-1, 0]), f) + datetime.timedelta(minutes=i+1)))
+con.commit()
+
+cursor.close()
+con.close()  # db disconnect
